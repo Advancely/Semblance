a6e1bf8da207e16223c3de58cabc4d3571ae64ba
“jiayinchu” <yinchu_jia@kingdee.com>
2024-05-26 10:09:41 +0800

    fix: 修改

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as exphbs from 'express-handlebars';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
